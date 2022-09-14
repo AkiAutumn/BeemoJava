f2825719672777867cc148e86d1d25a9b3b02e2c
BuildTools <unconfigured@null.spigotmc.org>
2022-09-14 21:46:21 +0200

    Test Slash Command copy-pasted

--- src/main/java/me/beemo/DiscordBot.java
+++ src/main/java/me/beemo/DiscordBot.java
@@ -4,8 +4,16 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
-public class DiscordBot {
+import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
+
+public class DiscordBot extends ListenerAdapter {
 
     public static void main(String[] args){
 
@@ -14,7 +22,40 @@ public class DiscordBot {
 
         JDA bot = JDABuilder.createDefault(dotenv.get("TOKEN"))
                 .setActivity(Activity.watching("Cyberpunk Edgerunners"))
+                .addEventListeners(new DiscordBot())
                 .build();
 
+        // These commands might take a few minutes to be active after creation/update/delete
+        CommandListUpdateAction commands = bot.updateCommands();
+
+        // Simple reply commands
+        commands.addCommands(
+                Commands.slash("say", "Makes the bot say what you tell it to")
+                        .addOption(STRING, "content", "What the bot should say", true) // you can add required options like this too
+        );
+
+        // Send the new set of commands to discord, this will override any existing global commands with the new set provided here
+        commands.queue();
+    }
+
+
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
+    {
+        // Only accept commands from guilds
+        if (event.getGuild() == null)
+            return;
+        switch (event.getName())
+        {
+            case "say":
+                say(event, event.getOption("content").getAsString()); // content is required so no null-check here
+                break;
+            default:
+                event.reply("I can't handle that command right now :(").setEphemeral(true).queue();
+        }
+    }
+
+    public void say(SlashCommandInteractionEvent event, String content)
+    {
+        event.reply(content).queue(); // This requires no permissions!
     }
 }
--- src/main/java/me/beemo/commands/colorRoleCommand.java
+++ src/main/java/me/beemo/commands/colorRoleCommand.java
@@ -1,4 +1,11 @@
 package me.beemo.commands;
 
+import net.dv8tion.jda.api.JDA;
+
 public class colorRoleCommand {
+
+    public static void colorRoleCommand(JDA bot){
+
+    }
+
 }
