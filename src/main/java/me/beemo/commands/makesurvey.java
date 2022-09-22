d3665b301af1ade31bd3481024ef3865ad9f103b
someKumo <simplykumo@gmail.com>
2022-09-22 12:55:29 +0200

    updated with additional conditions and its now removing 📝 afterwards

--- src/main/java/me/beemo/commands/makesurvey.java
+++ src/main/java/me/beemo/commands/makesurvey.java
@@ -2,24 +2,44 @@ package me.beemo.commands;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 
 public class makesurvey extends ListenerAdapter {
     public void onMessageReceived(MessageReactionAddEvent event) {
+        boolean check;
         if (event.getReaction().equals("📝")) {
-            Message message = event.getChannel().getHistory().getMessageById(event.getMessageId());
-            if (message != null) {
-                message.addReaction(Emoji.fromUnicode("U+274C")).queue();
-                message.addReaction(Emoji.fromUnicode("U+2705")).queue();
-            }
+            //when message has 📝 then continue
+            check = true;
+        } else {
+            //when message has not 📝 then abort
+            check = false;
+        }
 
+        if (event.getReaction().equals("✅")){
+            //when message has ✅ then abort
+            check = false;
+        } else {
+            //when message has not ✅ then continue
+            check = true;
+        }
+
+        if (event.getReaction().equals("❌")){
+            //when message has ❌ then abort
+            check = false;
+        } else {
+            //when message has not ❌ then continue
+            check = true;
+        }
+
+        if (check == true) {
+            Message message = event.getChannel().getHistory().getMessageById(event.getMessageId());
+            message.addReaction(Emoji.fromUnicode("U+274C")).queue();
+            message.addReaction(Emoji.fromUnicode("U+2705")).queue();
+            message.removeReaction(Emoji.fromUnicode("U+1F4DD")).queue();
+        } else {
+            return;
         }
     }
-
-    private void onMessageReceived(SlashCommandInteractionEvent event) {
-    }
-
 }
