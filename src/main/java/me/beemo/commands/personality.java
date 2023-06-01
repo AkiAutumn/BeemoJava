6c2f9a61e3a5d0e774f9d87644e75a896bd9e6fb
Aki <imdreamy@icloud.com>
2023-06-01 22:45:18 +0200

    personality tweaks

--- src/main/java/me/beemo/commands/personality.java
+++ src/main/java/me/beemo/commands/personality.java
@@ -1,12 +1,22 @@
 package me.beemo.commands;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.json.simple.JSONObject;
 
 import static me.beemo.DiscordBot.config;
 
 public class personality {
     public static void beemoSetPersonality(SlashCommandInteractionEvent event, String personality){
-        config.put("personality", personality);
-        event.reply("I updated my personality!").setEphemeral(true).queue();
+        if(personality != null) {
+            config.put("personality", personality);
+            event.reply("I updated my personality!").setEphemeral(true).queue();
+        } else {
+            JSONObject jsonObject = (JSONObject) config.get("personality");
+            if(jsonObject != null) {
+                event.reply("Current personality: " + jsonObject.toJSONString()).setEphemeral(true).queue();
+            } else {
+                event.reply("No config entry for a personality. Using default personality.").setEphemeral(true).queue();
+            }
+        }
     }
 }
