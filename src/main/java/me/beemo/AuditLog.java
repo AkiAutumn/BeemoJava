bb26dd7b47fcce87aac6cc144b627380e6a5027f
Aki <94296512+AkiAutumn@users.noreply.github.com>
2023-10-27 18:14:54 +0200

    custom audit log

--- src/main/java/me/beemo/AuditLog.java
+++ src/main/java/me/beemo/AuditLog.java
@@ -1,55 +1,59 @@
 package me.beemo;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.channel.ChannelType;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.entities.channel.middleman.AudioChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleRemoveEvent;
-import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.internal.handle.GuildRoleUpdateHandler;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 import static me.beemo.DiscordBot.*;
-import static me.beemo.DiscordBot.humanReadableByteCountBin;
 
 public class AuditLog extends ListenerAdapter {
 
     @Override
     public void onGuildMemberRoleAdd(GuildMemberRoleAddEvent event) {
-        EmbedBuilder embed = new EmbedBuilder();
-        embed.setTitle("Role(s) added");
-        embed.setThumbnail(event.getMember().getAvatarUrl());
-        embed.addField("User:", event.getMember().getAsMention(), true);
-        embed.addField("Role(s):", event.getRoles().toString(), true);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        embed.setFooter(dtf.format(now));
-        embed.setColor(Color.pink);
+        if(!sleeping) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("Role added");
+            embed.setImage(event.getMember().getAvatarUrl());
+            embed.addField("User:", event.getMember().getAsMention(), true);
+            embed.addField("Role:", roleString(event.getRoles()), true);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            embed.setFooter(dtf.format(now));
+            embed.setColor(Color.pink);
 
-        getAuditLogChannel().sendMessageEmbeds(embed.build()).queue();
+            getAuditLogChannel().sendMessageEmbeds(embed.build()).queue();
+        }
     }
 
     @Override
     public void onGuildMemberRoleRemove(GuildMemberRoleRemoveEvent event) {
-        EmbedBuilder embed = new EmbedBuilder();
-        embed.setTitle("Role(s) removed");
-        embed.setThumbnail(event.getMember().getAvatarUrl());
-        embed.addField("User:", event.getMember().getAsMention(), true);
-        embed.addField("Role(s):", event.getRoles().toString(), true);
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
-        embed.setFooter(dtf.format(now));
-        embed.setColor(Color.pink);
+        if (!sleeping) {
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setTitle("Role removed");
+            embed.setImage(event.getMember().getAvatarUrl());
+            embed.addField("User:", event.getMember().getAsMention(), true);
+            embed.addField("Role:", roleString(event.getRoles()), true);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+            LocalDateTime now = LocalDateTime.now();
+            embed.setFooter(dtf.format(now));
+            embed.setColor(Color.pink);
 
-        getAuditLogChannel().sendMessageEmbeds(embed.build()).queue();
+            getAuditLogChannel().sendMessageEmbeds(embed.build()).queue();
+        }
+    }
+
+    String roleString(java.util.List<Role> list) {
+        ArrayList<String> roleArray = new ArrayList<>();
+        for(Role role : list) {
+            roleArray.add(role.getAsMention());
+        }
+        return String.join(", ", roleArray);
     }
 }
